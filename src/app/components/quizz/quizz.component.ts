1a45a9b83c051f47e5811b9e1225610275b2c23c
Lucas Santos Cunha <54358807+lscunha@users.noreply.github.com>
2022-11-05 18:47:54 -0300

    Add files via upload

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../assets/questions/dataquiz.json";
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+
+
+export class QuizzComponent implements OnInit {
+  title:string = ""
+
+  questions: any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected: string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number =0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+  }
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex>this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer: string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz_questions.resuts[finalAnswer as keyof typeof quizz_questions.results]
+
+      //verificar opção ganhadora
+
+      console.log(this.answers)
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+    ['A','A','B','A']
+    const result = anwsers.reduce((previous,current, i,arr)=>{
+      if(
+        arr.filter(item => item=== previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      }else{
+        return current
+      }
+    })
+    return result
+  }
+
+}
